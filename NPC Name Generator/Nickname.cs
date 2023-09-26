b24f0b25aeb967455798a1d13aad66fdc0ad691c
trapersky <tomasz.zacharski@gmail.com>
2023-09-26 17:43:38 +0200

    Dodaj pliki projektów.

---
+++ NPC Name Generator/Nickname.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPC_Name_Generator
+{
+    enum Nickname
+    {
+        Black,
+        Midnight,
+        Orchid,
+        Knife,
+        Shadowspear,
+        Boots,
+        Arrow,
+        Quicksilver,
+        Magister,
+        Professor,
+        Pale,
+        Orange,
+        Hunter,
+        Tracker,
+        Belt,
+        Cat,
+        Dog,
+        Nightingale,
+        Parrot,
+        Bird,
+        Finger,
+        Silver,
+        Gold,
+        Needle,
+        Jack,
+        Ice,
+        Gallows,
+        Executioner,
+        Magic,
+        Traveller,
+        Gloves,
+        Rum,
+        Red,
+        Shadow,
+        Monkey,
+        Antlers,
+        Whale,
+        Captain,
+        Stone,
+        Green,
+        Stranger,
+    }
+}
